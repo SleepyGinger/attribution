fd0868d331a77bff9620130d10c863f0ac4b4857
Dan <Salazar@nyu.edu>
2018-03-11 14:41:37 -0700

    loops through extended results

--- ols_regression.py
+++ ols_regression.py
@@ -1,31 +1,61 @@
 import numpy as np
 import statsmodels.formula.api as smf
+import pandas as pd
 
 
-def ols_regression(df, results):
-    features = list(df)
-    rsquared=[]
-    rsquared_adj=[]
-    fvalue=[]
-    Log_Likelihood=[]
-    aic=[]
-    bic=[]
+def ols_regression(df, results, extended_results=False):
     
+    features = []
+    rejected = []
+    
+    #making sure we are working with numbers
+    for i in df:
+        if df[i].dtype == float:
+            rejected.append(i)
+        elif df[i].dtype == int:
+            features.append(i)
+        else:
+            rejected.append(i)
+            
+    if extended_results==True:
+        all_results=['result_binary', 'result_raw', 'result_scaler', 
+                     'result_L2_norm', 'result_minmaxscaler', 'result_dummy']
+    elif extended_results==False:
+        all_results=results
+
+    colNames=('Feature', 'Result', 'R-squared', 'Adj. R-squared', 'F-statistic', 
+              'Log-Likelihood', 'Akaike', 'Bayesian')
+    
+    results_df = pd.DataFrame(columns=colNames)
 
     for i in features:
-        X = df[i]
-        Y = df[results]
-        x = np.asarray(X)
-        y = np.asarray(Y)
-
-        OLS_Regression = smf.OLS(y,x).fit()
-        rsquared.append(format(OLS_Regression.rsquared, '.2f'))
-        rsquared_adj.append(format(OLS_Regression.rsquared_adj, '.2f'))
-        fvalue.append(format(OLS_Regression.fvalue, '.2f'))
-        Log_Likelihood.append(format(OLS_Regression.llf, '.2f'))
-        aic.append(format(OLS_Regression.aic, '.2f'))
-        bic.append(format(OLS_Regression.bic, '.2f'))
+        for e in all_results:
+            
+            rsquared=[]
+            rsquared_adj=[]
+            fvalue=[]
+            Log_Likelihood=[]
+            aic=[]
+            bic=[]   
         
-    return pd.DataFrame(zip(features, rsquared, rsquared_adj, fvalue, Log_Likelihood,aic, bic), 
-                        columns=['Feature', 'R-squared', 'Adj. R-squared', 'F-statistic', 
-                        'Log-Likelihood', 'Akaike', 'Bayesian']).sort_values(['R-squared'], ascending=False)
+            y = np.asarray(df[e])
+            
+            x = np.asarray(df[i])
+
+            OLS_Regression = smf.OLS(y,x).fit()
+
+            rsquared.append(format(OLS_Regression.rsquared, '.2f'))
+            rsquared_adj.append(format(OLS_Regression.rsquared_adj, '.2f'))
+            fvalue.append(format(OLS_Regression.fvalue, '.2f'))
+            Log_Likelihood.append(format(OLS_Regression.llf, '.2f'))
+            aic.append(format(OLS_Regression.aic, '.2f'))
+            bic.append(format(OLS_Regression.bic, '.2f'))
+            
+            series = [[i, e, rsquared, rsquared_adj, 
+                               fvalue, Log_Likelihood,aic, bic]]
+            
+            temp_df = pd.DataFrame(data=series, columns = colNames)
+            
+            results_df = results_df.append(temp_df, ignore_index=True)
+            
+    return results_df.sort_values(['R-squared'], ascending=False)
