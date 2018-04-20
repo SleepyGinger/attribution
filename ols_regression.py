6068dda5251b5c588fe95425a7462b83cc64e6a0
Dan <Salazar@nyu.edu>
2018-04-19 17:52:21 -0700

    added linear function

--- ols_regression.py
+++ ols_regression.py
@@ -3,7 +3,7 @@ import statsmodels.formula.api as smf
 import pandas as pd
 
 
-def ols_regression(df, results, extended_results=False):
+def regressions(df, results, extended_results=False):
     
     features = []
     rejected = []
@@ -22,40 +22,80 @@ def ols_regression(df, results, extended_results=False):
                      'result_L2_norm', 'result_minmaxscaler', 'result_dummy']
     elif extended_results==False:
         all_results=results
-
-    colNames=('Feature', 'Result', 'R-squared', 'Adj. R-squared', 'F-statistic', 
-              'Log-Likelihood', 'Akaike', 'Bayesian')
-    
-    results_df = pd.DataFrame(columns=colNames)
-
-    for i in features:
-        for e in all_results:
-            
-            rsquared=[]
-            rsquared_adj=[]
-            fvalue=[]
-            Log_Likelihood=[]
-            aic=[]
-            bic=[]   
         
-            y = np.asarray(df[e])
-            
-            x = np.asarray(df[i])
+    colNames=('Feature', 'Result', 'R-squared', 'Adj. R-squared', 'F-statistic', 
+      'Log-Likelihood', 'Akaike', 'Bayesian')
+    ols_df = pd.DataFrame(columns=colNames)
 
-            OLS_Regression = smf.OLS(y,x).fit()
+        
+    def ols(features, all_results, ols_df):
 
-            rsquared.append(format(OLS_Regression.rsquared, '.2f'))
-            rsquared_adj.append(format(OLS_Regression.rsquared_adj, '.2f'))
-            fvalue.append(format(OLS_Regression.fvalue, '.2f'))
-            Log_Likelihood.append(format(OLS_Regression.llf, '.2f'))
-            aic.append(format(OLS_Regression.aic, '.2f'))
-            bic.append(format(OLS_Regression.bic, '.2f'))
+        for i in features:
+            for e in all_results:
+
+                rsquared=[]
+                rsquared_adj=[]
+                fvalue=[]
+                Log_Likelihood=[]
+                aic=[]
+                bic=[]   
+
+                y = np.asarray(df[e])
+
+                x = np.asarray(df[i])
+
+                OLS_Regression = smf.OLS(y,x).fit()
+
+                rsquared.append(format(OLS_Regression.rsquared, '.2f'))
+                rsquared_adj.append(format(OLS_Regression.rsquared_adj, '.2f'))
+                fvalue.append(format(OLS_Regression.fvalue, '.2f'))
+                Log_Likelihood.append(format(OLS_Regression.llf, '.2f'))
+                aic.append(format(OLS_Regression.aic, '.2f'))
+                bic.append(format(OLS_Regression.bic, '.2f'))
+
+                series = [[i, e, rsquared, rsquared_adj, 
+                                   fvalue, Log_Likelihood,aic, bic]]
+
+                temp_df = pd.DataFrame(data=series, columns = colNames)
+
+                ols_df = ols_df.append(temp_df, ignore_index=True)
             
-            series = [[i, e, rsquared, rsquared_adj, 
-                               fvalue, Log_Likelihood,aic, bic]]
+        return ols_df.sort_values(['R-squared'], ascending=False)
+    
+    linearcol=('Feature', 'Result', 'Coefficients', 'MSE', 'Variance')
+    linear_df = pd.DataFrame(columns=linearcol)
+
+    def linear(features, all_results, linear_df):
+        for i in features:
+            for e in all_results:
+                
+                coefficients = []
+                mse = []
+                variance = []
+
+                y = np.asarray(df[e]).reshape(-1, 1)
+
+                x = np.asarray(df[i]).reshape(-1, 1)
+
+                na_features=missingvalues_check(df, no_nas=True)
+                X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
+
+                regr.fit(X_train, y_train)
+
+                y_pred = regr.predict(X_test)
+
+                coefficients.append(regr.coef_)
+                mse.append(mean_squared_error(y_test, y_pred))
+                variance.append(r2_score(y_test, y_pred))
+
+                series = [[i, e, coefficients, mse, variance]]
+
+                ltemp_df = pd.DataFrame(data=series, columns = linearcol)
+
+                linear_df = linear_df.append(ltemp_df, ignore_index=True)
             
-            temp_df = pd.DataFrame(data=series, columns = colNames)
-            
-            results_df = results_df.append(temp_df, ignore_index=True)
-            
-    return results_df.sort_values(['R-squared'], ascending=False)
+        return linear_df
+    
+    ltemp_df=linear(features, all_results, linear_df)
+    
+    return ltemp_df
